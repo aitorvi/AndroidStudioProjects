fe20ce0f5ee32aecbb1ee80783bcd449df8473e5
aitorvi <aitorvillalon@gmail.com>
2017-03-20 14:18:03 +0000

    Definitivo2

---
+++ Definitivo/app/src/main/java/com/example/aitor/prueba/AddActividadesActivity.java
@@ -0,0 +1,148 @@
+package com.example.aitor.prueba;
+
+import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
+
+/**
+ * Created by Aitor on 16/03/2017.
+ */
+public class AddActividadesActivity extends Activity implements View.OnClickListener {
+
+    private Button addTodoBtn;
+    private EditText subjectEditText, et2, et3;
+    private RadioButton r1, r2, r3, r4, r5;
+    TextView et1;
+    EditText date,time;
+    DatePickerDialog datePickerDialog;
+    TimePickerDialog timePickerDialogcker;
+
+
+    private DBManager dbManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setTitle("Añadir Tarea");
+
+        setContentView(R.layout.activity_add_record);
+
+        subjectEditText = (EditText) findViewById(R.id.subject_edittext);
+        et2 = (EditText) findViewById(R.id.et2);
+        et3 = (EditText) findViewById(R.id.et3);
+        r1 = (RadioButton) findViewById(R.id.r1);
+        r2 = (RadioButton) findViewById(R.id.r4);
+        r3 = (RadioButton) findViewById(R.id.r3);
+        r4 = (RadioButton) findViewById(R.id.r4);
+        r5 = (RadioButton) findViewById(R.id.r5);
+        date = (EditText) findViewById(R.id.date);
+        time =  (EditText) findViewById(R.id.time);
+
+
+
+
+
+        addTodoBtn = (Button) findViewById(R.id.add_record);
+
+        dbManager = new DBManager(this);
+        dbManager.open();
+        addTodoBtn.setOnClickListener(this);
+
+    }
+
+//Se recogen los datos seleccionados
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.add_record:
+
+                final String name = subjectEditText.getText().toString();
+                String desc = "Hecho";
+                String prioridad = "Baja";
+                final String fecha = "";
+                final String hora = "";
+
+
+                if (r1.isChecked() == true) {
+                    desc = "Pendiente";
+                }
+
+
+                if (r3.isChecked() == true) {
+                    prioridad = "Baja";
+                }
+
+                if (r5.isChecked() == true) {
+                    prioridad = "alta";
+                }
+// Se inserta en la BBDD datos recogidos
+                dbManager.insert(name, desc,prioridad,fecha,hora);
+// Se vuelva a la actividad principal
+                Intent main = new Intent(AddActividadesActivity.this, ListaActividadesActivity.class)
+                        .setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+                startActivity(main);
+                break;
+
+        }
+    }
+    public void addFecha(View v) {
+        // Abrir activity calendario y añadir fecha al et
+        final Calendar c = Calendar.getInstance();
+        int mYear = c.get(Calendar.YEAR); // current year
+        int mMonth = c.get(Calendar.MONTH); // current month
+        int mDay = c.get(Calendar.DAY_OF_MONTH); // current day
+        // date picker dialog
+        datePickerDialog = new DatePickerDialog(AddActividadesActivity.this,
+                new DatePickerDialog.OnDateSetListener() {
+
+                    @Override
+                    public void onDateSet(DatePicker view, int year,
+                                          int monthOfYear, int dayOfMonth) {
+                        // set day of month , month and year value in the edit text
+                        date.setText(dayOfMonth + "/"
+                                + (monthOfYear + 1) + "/" + year);
+
+                    }
+                }, mYear, mMonth, mDay);
+        datePickerDialog.show();    }
+
+    // abrir TimePicer y añadir hora a et
+    public void addHora(View v) {
+        Calendar mcurrentTime = Calendar.getInstance();
+        int hour = mcurrentTime.get(Calendar.HOUR_OF_DAY);
+        int minute = mcurrentTime.get(Calendar.MINUTE);
+        TimePickerDialog mTimePicker;
+        mTimePicker = new TimePickerDialog(AddActividadesActivity.this, new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
+                time.setText(selectedHour + ":" + selectedMinute);
+            }
+        }, hour, minute, true);//Yes 24 hour time
+        mTimePicker.setTitle("Select Time");
+        mTimePicker.show();   }
+
+    public void cancel (View v) {
+        finish();
+    }
+    // devuelve todos los componentes de la activity a su estado original
+    public void clear (View v) {
+        subjectEditText.setText("");
+        //r2.che
+        //r4.isSelected();
+        time.setText("");
+        date.setText("");
+    }
+}
---
+++ Definitivo/app/src/main/java/com/example/aitor/prueba/DBManager.java
@@ -0,0 +1,66 @@
+package com.example.aitor.prueba;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+
+/**
+ * Created by Aitor on 16/03/2017.
+ */
+
+public class DBManager {
+
+    private DatabaseHelper dbHelper;
+
+    private Context context;
+
+    private SQLiteDatabase database;
+
+    public DBManager(Context c) {
+        context = c;
+    }
+
+    public DBManager open() throws SQLException {
+        dbHelper = new DatabaseHelper(context);
+        database = dbHelper.getWritableDatabase();
+        return this;
+    }
+
+    public void close() {
+        dbHelper.close();
+    }
+
+    public void insert(String name, String desc,String prioridad, String fecha, String hora) {
+        ContentValues contentValue = new ContentValues();
+        contentValue.put(DatabaseHelper.SUBJECT, name);
+        contentValue.put(DatabaseHelper.DESC, desc);
+          contentValue.put(DatabaseHelper.PRIORIDAD, prioridad);
+          contentValue.put(DatabaseHelper.FECHA, fecha);
+         contentValue.put(DatabaseHelper.HORA, hora);
+        database.insert(DatabaseHelper.TABLE_NAME, null, contentValue);
+    }
+
+    public Cursor fetch() {
+        String[] columns = new String[] { DatabaseHelper._ID, DatabaseHelper.SUBJECT, DatabaseHelper.DESC };
+        Cursor cursor = database.query(DatabaseHelper.TABLE_NAME, columns, null, null, null, null, null);
+        if (cursor != null) {
+            cursor.moveToFirst();
+        }
+        return cursor;
+    }
+
+    public int update(long _id, String name, String desc) {
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(DatabaseHelper.SUBJECT, name);
+        contentValues.put(DatabaseHelper.DESC, desc);
+        int i = database.update(DatabaseHelper.TABLE_NAME, contentValues, DatabaseHelper._ID + " = " + _id, null);
+        return i;
+    }
+
+    public void delete(long _id) {
+        database.delete(DatabaseHelper.TABLE_NAME, DatabaseHelper._ID + "=" + _id, null);
+    }
+
+}
