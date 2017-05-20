a026f8780703229e310c008175c5fd6d127860b4
aitorvi <aitorvillalon@gmail.com>
2017-05-20 20:11:42 +0100

    2

--- Rutas/app/src/main/java/com/example/aitor/rutas/MapsActivity.java
+++ Rutas/app/src/main/java/com/example/aitor/rutas/MapsActivity.java
@@ -1,7 +1,10 @@
 package com.example.aitor.rutas;
 
+import android.graphics.Color;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -9,10 +12,19 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import static com.example.aitor.rutas.R.id.map;
+import static java.security.AccessController.getContext;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    public TextView mapaRecorrido;
+    public TextView mapaDificultad;
+    public TextView mapaKml;
+    public TextView mapaDescripcion;
+    public String kml;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,9 +32,33 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+                .findFragmentById(map);
         mapFragment.getMapAsync(this);
+
+        //Se recogen los datos de la anterior activity (hay que cambiarlo a un objeto)
+
+
+        mapaRecorrido = (TextView) findViewById(R.id.tvrecorrido);
+        mapaDificultad = (TextView) findViewById(R.id.tvdificultad);
+        mapaDescripcion = (TextView) findViewById(R.id.tvdescripcion);
+
+
+
+        Bundle bundle = getIntent().getExtras();
+        String recorrido=bundle.getString("recorrido");
+        String dificultad=bundle.getString("dificultad");
+       String descripcion=bundle.getString("descripcion");
+        kml=bundle.getString("kml");
+
+//Se rellenan los edittext
+        mapaRecorrido.setText(recorrido);
+        mapaDificultad.setText(dificultad);
+        mapaDescripcion.setText(descripcion);
+
     }
+
+
+
 
 
     /**
@@ -37,10 +73,28 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
+        mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        //kml.substring()
+        LatLng inicio = new LatLng(28.61, -13.92);
+        mMap.addMarker(new MarkerOptions().position(inicio).title("Fuerteventura"));
+
+
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(fuerteventura));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(inicio, 8));
+        mMap.addPolyline(new PolylineOptions()
+                        .color(Color.YELLOW)
+                .add(new LatLng(28.61,28.62),new LatLng(28.61,28.62))
+                .clickable(true)
+
+
+        );
+
+
+
+
+       // mMap.addPolyline(loadGeoJson(
+         //       'https://storage.googleapis.com/mapsdevsite/json/google.json'));
+
     }
 }
