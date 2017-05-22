52cb5b14468c1e4ad0e823fca951072566dd7460
aitorvi <aitorvillalon@gmail.com>
2017-05-22 16:01:53 +0100

    2

--- Rutas/app/src/main/java/com/example/aitor/rutas/MapsActivity.java
+++ Rutas/app/src/main/java/com/example/aitor/rutas/MapsActivity.java
@@ -14,6 +14,11 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.PolylineOptions;
 
+import java.util.List;
+
+import static android.R.attr.path;
+import static android.R.attr.strokeColor;
+import static com.example.aitor.rutas.R.id.large_icon_uri;
 import static com.example.aitor.rutas.R.id.map;
 import static java.security.AccessController.getContext;
 
@@ -24,7 +29,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public TextView mapaDificultad;
     public TextView mapaKml;
     public TextView mapaDescripcion;
+    public String kmlx;
+    public String kmly;
+    public List<LatLng> prueba;
     public String kml;
+    double pointY[]={105.45526,105.57364,105.53505,105.45523,105.51962,105.77320};
+    double pointX[]={9.99222,9.88347,9.84184,9.77197,9.55501,9.67768,9};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,11 +59,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         String dificultad=bundle.getString("dificultad");
        String descripcion=bundle.getString("descripcion");
         kml=bundle.getString("kml");
-
 //Se rellenan los edittext
         mapaRecorrido.setText(recorrido);
         mapaDificultad.setText(dificultad);
         mapaDescripcion.setText(descripcion);
+
+        //
+
+
 
     }
 
@@ -78,23 +91,30 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         //kml.substring()
         LatLng inicio = new LatLng(28.61, -13.92);
         mMap.addMarker(new MarkerOptions().position(inicio).title("Fuerteventura"));
+        String [] listax = kmlx.split("|");
+        String [] listay=kmly.split("|");
 
+          Double x= Double.parseDouble(listax[0]) ;
+        Double y =Double.parseDouble(listay[0]);
 
-        //mMap.moveCamera(CameraUpdateFactory.newLatLng(fuerteventura));
+        prueba.add(0,new LatLng(x,y));
+
+         mMap.moveCamera(CameraUpdateFactory.newLatLng(inicio));
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(inicio, 8));
-        mMap.addPolyline(new PolylineOptions()
-                        .color(Color.YELLOW)
-                .add(new LatLng(28.61,28.62),new LatLng(28.61,28.62))
-                .clickable(true)
+        mMap.addPolyline(new PolylineOptions().addAll(prueba));
+
+        // .add(new LatLng(),new LatLng(29,13.62)
 
 
-        );
-
+        //mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(-33.8256, 151.2395), 10));
 
 
 
-       // mMap.addPolyline(loadGeoJson(
-         //       'https://storage.googleapis.com/mapsdevsite/json/google.json'));
 
     }
-}
+
+
+
+    }
+
+
